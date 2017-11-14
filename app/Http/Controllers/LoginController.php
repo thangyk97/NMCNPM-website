0e2167fa4c954957a8bd0041551df42c1dfa6038
thangyk97 <thangyk97@gmail.com>
2017-11-14 14:46:56 +0700

    de code json to array and insert into database

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,17 +4,34 @@ namespace App\Http\Controllers;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use DB;
 
 class LoginController extends Controller
 {
 
     public function postForm(Request $request)
     {
-        if($request->name == "blabla") {
-            echo "true";
-        } else {
-            echo "false";
-        }
+
+        // $crazy = '[
+        //     {
+        //       "password": "Chaunceymouth",
+        //       "id": 0,
+        //       "name": "Brielle"
+        //     },
+        //     {
+        //       "password": "East Cale",
+        //       "id": 1,
+        //       "name": "Ressie"
+        //     }
+        //   ]';
+        // $user_array = json_decode($crazy, true);
+
+        // foreach ($user_array as $row) {
+        //     DB::insert('insert into user values (?, ?, ?)', 
+        //     array($row['id'], '"'.$row['name'].'"', '"'.$row['password'].'"'));
+
+        // }
+
     }
 
 }
