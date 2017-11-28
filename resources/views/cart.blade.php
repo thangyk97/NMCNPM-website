8fa649f031e5936ec49f8302b46136d0fa05d2c9
thang nguyen dinh <thangyk97@gmail.com>
2017-11-28 16:30:47 +0700

    setting display cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -13,6 +13,8 @@
 				  <li class="active">Shopping Cart</li>
 				</ol>
 			</div>
+
+
 			<div class="table-responsive cart_info">
 				<table class="table table-condensed">
 					<thead>
@@ -26,82 +28,42 @@
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td class="cart_product">
-								<a href=""><img src="images/cart/one.png" alt=""></a>
-							</td>
-							<td class="cart_description">
-								<h4><a href="">Colorblock Scuba</a></h4>
-								<p>Web ID: 1089772</p>
-							</td>
-							<td class="cart_price">
-								<p>$59</p>
-							</td>
-							<td class="cart_quantity">
-								<div class="cart_quantity_button">
-									<a class="cart_quantity_up" href=""> + </a>
-									<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
-									<a class="cart_quantity_down" href=""> - </a>
-								</div>
-							</td>
-							<td class="cart_total">
-								<p class="cart_total_price">$59</p>
-							</td>
-							<td class="cart_delete">
-								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
-							</td>
-						</tr>
 
-						<tr>
-							<td class="cart_product">
-								<a href=""><img src="images/cart/two.png" alt=""></a>
-							</td>
-							<td class="cart_description">
-								<h4><a href="">Colorblock Scuba</a></h4>
-								<p>Web ID: 1089772</p>
-							</td>
-							<td class="cart_price">
-								<p>$59</p>
-							</td>
-							<td class="cart_quantity">
-								<div class="cart_quantity_button">
-									<a class="cart_quantity_up" href=""> + </a>
-									<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
-									<a class="cart_quantity_down" href=""> - </a>
-								</div>
-							</td>
-							<td class="cart_total">
-								<p class="cart_total_price">$59</p>
-							</td>
-							<td class="cart_delete">
-								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td class="cart_product">
-								<a href=""><img src="images/cart/three.png" alt=""></a>
-							</td>
-							<td class="cart_description">
-								<h4><a href="">Colorblock Scuba</a></h4>
-								<p>Web ID: 1089772</p>
-							</td>
-							<td class="cart_price">
-								<p>$59</p>
-							</td>
-							<td class="cart_quantity">
-								<div class="cart_quantity_button">
-									<a class="cart_quantity_up" href=""> + </a>
-									<input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
-									<a class="cart_quantity_down" href=""> - </a>
-								</div>
-							</td>
-							<td class="cart_total">
-								<p class="cart_total_price">$59</p>
-							</td>
-							<td class="cart_delete">
-								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
-							</td>
-						</tr>
+						@if($cart != null)
+
+							@foreach ($cart->items as $storedItem)
+								<tr>
+									<td class="cart_product">
+										<a href=""><img src="{{URL::asset($storedItem['item']->link_img)}}" alt=""></a>
+									</td>
+									<td class="cart_description">
+										<h4><a href="">{{$storedItem['item']->name}}</a></h4>
+										<p>Web ID: {{$storedItem['item']->id}}</p>
+									</td>
+									<td class="cart_price">
+										<p>{{'$'.$storedItem['item']->price}}</p>
+									</td>
+									<td class="cart_quantity">
+										<div class="cart_quantity_button">
+											<a class="cart_quantity_up" href="{{URL('cart')}}"> + </a>
+											<input class="cart_quantity_input" type="text" name="quantity" value="{{$storedItem['qty']}}" autocomplete="off" size="2">
+											<a class="cart_quantity_down" href=""> - </a>
+										</div>
+									</td>
+									<td class="cart_total">
+										<p class="cart_total_price">{{'$'.$storedItem['price']}}</p>
+									</td>
+									<td class="cart_delete">
+										<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+									</td>
+								</tr>
+							@endforeach
+						@else
+							<tr>
+								<h3>empty card</h3>
+							</tr>
+
+						@endif
 					</tbody>
 				</table>
 			</div>
